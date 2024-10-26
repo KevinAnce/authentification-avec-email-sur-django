373caf73307091c1d27fb421df4b13b36906054e
TIAKOUANG DJOU KEVIN <djoukevin1469@gmail.com>
2024-10-26 09:59:45 +0100

    fix: modify settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -132,10 +132,10 @@ OTP_VALID_MINUTES = 10
 
 # Email settings
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.ConsoleBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_FROM = "djoukevin1469@gmail.com"
+EMAIL_FROM = "******************@gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "djoukevin1469@gmail.com"
-EMAIL_HOST_PASSWORD = "uedfnqoaxvddnpzx"
+EMAIL_HOST_USER = "*************************@gmail.com"
+EMAIL_HOST_PASSWORD = "*********************************"
 EMAIL_USE_TLS = True
